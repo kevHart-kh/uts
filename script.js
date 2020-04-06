33c1c8a900e44b04b3c1ed0873aad0d81ce8f8ed
kevinhartono <kevinhartono2000b@gmail.com>
2020-04-06 15:41:16 +0700

    list pinjaman sudah bisa

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var semua_jumlah = [1];
 var semua_waktu = [1];
 counter = 0;
 
+$( document ).on( "mobileinit", function() {
+ 
+    // We want popups to cover the page behind them with a dark background
+    $.mobile.popup.prototype.options.overlayTheme = "b";
+   
+    // Set a namespace for jQuery Mobile data attributes
+    $.mobile.ns = "jqm-";
+  });
 
 function input_pinjaman(){
     var jumlah = document.getElementById("jumlah_pinjaman");
@@ -26,8 +34,11 @@ function input_pinjaman(){
         semua_waktu[0]=waktu;
         counter++;
     }else{
-        semua_jumlah=push(jumlah);
-        semua_waktu=push(waktu);
+        // semua_jumlah=push(jumlah);
+        // semua_waktu=push(waktu);
+
+        semua_jumlah[counter]=jumlah;
+        semua_waktu[counter]=waktu;
         counter++;
     }
 
@@ -37,4 +48,44 @@ function input_pinjaman(){
     var stringHasil=""+jumlah+" + ( " + jumlah +" * " + bunga + "% * " + waktu + " ) = " + hasil;
     
     document.getElementById("hasil-pengembalian").innerHTML = stringHasil;
+
+
+    document.getElementById("jumlah_pinjaman").value="";;
+    document.getElementById("waktu_pinjaman").value="6";
+    
+
+
+    update_listPinjaman();
+}
+
+function update_listPinjaman(){
+    var list = document.getElementById("list-pinjaman");
+
+    var html="";
+
+    for (let index = 0; index < counter; index++) {
+        html += '<div data-role="collapsible"><h3>pinjaman:'+ semua_jumlah[index] + 'waktu:' + semua_waktu[index] +'</h3><ul data-role=listview>'
+
+        var waktu = semua_waktu[index];
+        var jumlah = semua_jumlah[index];
+        var bunga;
+        if (waktu<=6) {
+            bunga=2;
+        }else if(waktu<=12){
+            bunga=1.5;
+        }else{
+            bunga=1;
+        }
+
+        var hasil = jumlah+(jumlah*bunga/100*waktu);
+        
+        
+        var stringHasil=""+jumlah+" + ( " + jumlah +" * " + bunga + "% * " + waktu + " ) = " + hasil;
+        
+        html += stringHasil;
+        html += '</ul></div>'    
+    }
+
+    list.innerHTML=html;
+    
 }
